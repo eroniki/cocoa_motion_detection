07f260599e4e5a0b476fe497c617ee8812cfad80
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2016-04-08 16:49:07 -0400

    Add gradient based motion detection
    has a lot of problems

--- motion_detection_combined.m
+++ motion_detection_combined.m
@@ -1,5 +1,6 @@
 function [foreground] = motion_detection_combined(frameSequence, p)
 % TODO: Add explicit explanations here
+% TODO: Check the formulations for the deviation and the variance
 % 0.2989 * R + 0.5870 * G + 0.1140 * B 
 coefficient.R = 0.2989;
 coefficient.G = 0.5870;
@@ -44,20 +45,74 @@ for i=1:frameNumber
     clusterX = cluster(gmfit,tt);
     clustered = reshape(clusterX,[y,x]);
     [~, minIdx] = min(gmfit.ComponentProportion);
-    foreground(i).gmm_foreground = clustered == minIdx;
+    foreground(i).gmm_foreground = zeros(y, x, 'double');
+    foreground(i).gmm_foreground(clustered == minIdx) = 1;
     foreground(i).gmm_percent = sum(foreground(i).gmm_foreground(:))/numel(foreground(i).gmm_foreground(:));
     disp('Finish Color-Based Background Subtraction');
     %% Gradient-based Background Subtraction
     % TODO: Implement this method
-%     disp('Start Gradient-Based Background Subtraction');
-
-%     disp('Finish Gradient-Based Background Subtraction');
+    disp('Start Gradient-Based Background Subtraction');
+    foreground(i).gradient.mu = zeros(y, x, 'double');
+    foreground(i).gradient.variance = zeros(y, x, 'double');
+    gray_mu = zeros(1, k);
+    gray_variance = zeros(1, k);
     
+    figure(88); imagesc(foreground(i).gradient.mu);
+    
+    for t=1:k
+        gray_mu(i) = gmfit.mu(t,1) * coefficient.R + gmfit.mu(t,2) * coefficient.G + gmfit.mu(t,3) * coefficient.B;
+        gray_variance(i) = coefficient.R^2 * gmfit.Sigma(:,1,t) + coefficient.G^2 * gmfit.Sigma(:,2,t) + coefficient.B^2 * gmfit.Sigma(:,3,t);
+        foreground(i).gradient.mu(clustered == t) = gray_mu(t);
+        foreground(i).gradient.variance(clustered == t) = gray_variance(t);
+        figure(89); imagesc(foreground(i).gradient.mu); pause;
+    
+    end
+    figure(90); imagesc(foreground(i).gradient.mu);
+    pause;
+    
+    [Gx, Gy] = imgradientxy(frameSequence(i).image_gray);
+    [Gmag, Gdir] = imgradient(Gx, Gy);
+    
+    distribution = zeros(y, x, 'double');
+    
+    cosGdir = cos(Gdir);
+    sinGdir = sin(Gdir);
+    
+    for row=2:y-1
+        for col=2:x-1       
+            [row, col, foreground(i).gradient.mu(row,col+1)]
+            
+            mu_fx = gray_mu(foreground(i).gradient.mu(row,col+1)) ...
+               - gray_mu(foreground(i).gradient.mu(row,col));
+            mu_fy = gray_mu(foreground(i).gradient.mu(row+1,col)) ...
+               - gray_mu(foreground(i).gradient.mu(row,col));
+            
+           rho = gray_variance(i) / (mu_fx(row,col)*mu_fy(row,col))^0.5;
+
+           variance_fx = gray_variance(foreground(i).gradient.variance(row,col+1)) ...
+                + gray_variance(foreground(i).gradient.variance(row,col)); 
+            
+            variance_fy = gray_variance(foreground(i).gradient.variance(row+1,col)) ...
+                + gray_variance(foreground(i).gradient.variance(row,col));
+           
+            z = ((Gmag(row, col) * cosGdir(row, col)-mu_fx)/variance_fx)^2 ...
+                -2*rho*((Gmag(row, col) * cosGdir(row, col)-mu_fx)/variance_fx) ...
+                * (Gmag(row, col) * sinGdir(row, col)-mu_fy)/variance_fy ...
+                + ((Gmag(row, col) * sinGdir(row, col)-mu_fy)/variance_fy)^2;
+            
+            distribution(y,x) = Gmag(row, col) / (2*pi*mu_fx(row,col)*mu_fy(row,col)*sqrt(1-rho^2)) ...
+                * exp(-1*z/(2*(1-rho^2)));
+        end
+    end
+    foreground(i).distribution = distribution;
+    disp('Finish Gradient-Based Background Subtraction');
+    assignin('base', 'foreground', foreground)
     %% Visualization of the Results
     figure(1);
     subplot(2,2,1); imshow(foreground(i).afd_image); title('AFD Result');
     subplot(2,2,2); imshow(foreground(i).gmm_foreground); title('Color-based BG Subtraction Result');
     subplot(2,2,3); imshow(foreground(i).gmm_foreground .* foreground(i).afd_image); title('Combined Result'); 
+    subplot(2,2,4); imshow(foreground(i).distribution);
     drawnow;
 end
 
